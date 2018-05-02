11c786ed4c333cf9f7ac0566ace7eba46a68a411
Justin Kufro <jkufro@andrew.cmu.edu>
2018-05-02 00:26:26 +0000

    fixed validator for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,5 @@ class Article < ApplicationRecord
 	scope :active, 	     -> { where('active = ?', true) }
     scope :alphabetical, -> { order('title') }
     
-    validate_presence_of :title, :content
+    validates_presence_of :title, :content
 end
